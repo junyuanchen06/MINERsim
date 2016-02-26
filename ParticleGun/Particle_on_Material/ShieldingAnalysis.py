abbbe1b74855f688a520ad2703d68dd7f7c5fbe3
shivakshar <shivakshar.yadavalli@gmail.com>
2016-02-25 21:43:52 -0600

    Update ShieldingAnalysis.py

--- ParticleGun/Particle_on_Material/ShieldingAnalysis.py
+++ ParticleGun/Particle_on_Material/ShieldingAnalysis.py
@@ -181,9 +181,141 @@ for line in file_gPolg:
 M_gPolg=np.array(M_gPolg)
 print ("gPolg Ready!")
 
-#Create Concrete Material Matrix
+#Create Polyethylene Material Matrix
 M_Pol=np.array([[M_nPoln,M_nPolg],[M_gPoln,M_gPolg]])
 print("Polyethylene Matrix Complete.")
+
+#nGdn
+file_nGdn=open('./Results_nGdn.txt')
+M_nGdn=[]
+for line in file_nGdn:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nGdn.append(row)
+M_nGdn=np.array(M_nGdn)
+print ("nGdn Ready!")
+
+#nGdg
+file_nGdg=open('./Results_nGdg.txt')
+M_nGdg=[]
+for line in file_nGdg:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nGdg.append(row)
+M_nGdg=np.array(M_nGdg)
+print ("nGdg Ready!")
+
+#gGdn
+file_gGdn=open('./Results_gGdn.txt')
+M_gGdn=[]
+for line in file_gGdn:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gGdn.append(row)
+M_gGdn=np.array(M_gGdn)
+print ("gGdn Ready!")
+
+#gGdg
+file_gGdg=open('./Results_gGdg.txt')
+M_gGdg=[]
+for line in file_gGdg:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gGdg.append(row)
+M_gGdg=np.array(M_gGdg)
+print ("gGdg Ready!")
+
+#Create Gadolinium Material Matrix
+M_Gd=np.array([[M_nGdn,M_nGdg],[M_gGdn,M_gGdg]])
+print("Gadolinium Matrix Complete.")
+
+#nGen
+file_nGen=open('./Results_nGen.txt')
+M_nGen=[]
+for line in file_nGen:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nGen.append(row)
+M_nGen=np.array(M_nGen)
+print ("nGen Ready!")
+
+#nGeg
+file_nGeg=open('./Results_nGeg.txt')
+M_nGeg=[]
+for line in file_nGeg:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nGeg.append(row)
+M_nGeg=np.array(M_nGeg)
+print ("nGeg Ready!")
+
+#gGen
+file_gGen=open('./Results_gGen.txt')
+M_gGen=[]
+for line in file_gGen:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gGen.append(row)
+M_gGen=np.array(M_gGen)
+print ("gGen Ready!")
+
+#gGeg
+file_gGeg=open('./Results_gGeg.txt')
+M_gGeg=[]
+for line in file_gGeg:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gGeg.append(row)
+M_gGeg=np.array(M_gGeg)
+print ("gGeg Ready!")
+
+#Create Germanium Material Matrix
+M_Ge=np.array([[M_nGen,M_nGeg],[M_gGen,M_gGeg]])
+print("Germanium Matrix Complete.")
+
+#nSin
+file_nSin=open('./Results_nSin.txt')
+M_nSin=[]
+for line in file_nSin:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nSin.append(row)
+M_nSin=np.array(M_nSin)
+print ("nSin Ready!")
+
+#nSig
+file_nSig=open('./Results_nSig.txt')
+M_nSig=[]
+for line in file_nSig:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_nSig.append(row)
+M_nSig=np.array(M_nSig)
+print ("nSig Ready!")
+
+#gSin
+file_gSin=open('./Results_gSin.txt')
+M_gSin=[]
+for line in file_gSin:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gSin.append(row)
+M_gSin=np.array(M_gSin)
+print ("gSin Ready!")
+
+#gSig
+file_gSig=open('./Results_gSig.txt')
+M_gSig=[]
+for line in file_gSig:
+    line.strip()
+    row=[float(elt) for elt in line.split()]
+    M_gSig.append(row)
+M_gSig=np.array(M_gSig)
+print ("gSig Ready!")
+
+#Create Germanium Material Matrix
+M_Si=np.array([[M_nSin,M_nSig],[M_gSin,M_gSig]])
+print("Silicon Matrix Complete.")
 
 print(" All Matrices Built.")
 
@@ -192,21 +324,23 @@ print(" All Matrices Built.")
 #Input Spectrum of Neutrons
 from scipy.stats import norm
 E = np.arange(10,10001,10)
+#I_spec_n=1000000000*maxwell.pdf((E+2000)/1.4,0,2000)
 #Gaussian Spectrum
 I_spec_n=(10**6)*norm.pdf(E,100,100)
-#I_spec_n=np.array(1000*[2000])
+#I_spec_n=np.array(1000*[0])
 print("Input Spectrum of Neutrons Built")
 
 
 #Input Spectrum of Gammas
-from scipy.stats import norm
+#from scipy.stats import norm
 E = np.arange(10,10001,10)
 #Flat Spectrum
 I_spec_g=np.array(1000*[2000])
+#I_spec_g=(100000000/E)+(10**3)
 print("Input Spectrum of Gamma Built")
 
 #Input Spectrum
-I_spec=np.array([I_spec_n,I_spec_g])
+#I_spec=np.array([I_spec_n,I_spec_g])
 
 #Compute Output Spectrum based length in cm
 def Layers(I_n,I_g,M,l):    
@@ -222,9 +356,9 @@ print("Build the Shielding scheme you wish to analyse")
 typs=[]
 thicks=[]
 flag=True
-dict={'1':"Pb",'2':"HDC",'3':"Water",'4':"Poly"}
+dict={'1':"Pb",'2':"HDC",'3':"Water",'4':"Poly",'5':"Gd","6":"Ge","7":"Si"}
 while(flag):
-  typ=input("Add a Layer\n Press 1 for Pb \n Press 2 for HDC \n Press 3 for Water \n Press 4 for Polyethylene \nInput: ")
+  typ=input("Add a Layer\n Press 1 for Pb \n Press 2 for HDC \n Press 3 for Water \n Press 4 for Polyethylene \n Press 5 for Gadolinium \n Press 6 for Germanium \n Press 7 for Silicon \nInput: ")
   thick=input("Input thickness in cm: ")
   typs.append(dict[typ])
   thicks.append(thick)
@@ -236,7 +370,7 @@ while(flag):
 
 O_spec_n,O_spec_g=I_spec_n,I_spec_g
 for i in range(len(typs)):
-    mater={'Pb':M_Pb,'HDC':M_Con,'Water':M_Wat, 'Poly': M_Pol}
+    mater={'Pb':M_Pb,'HDC':M_Con,'Water':M_Wat, 'Poly': M_Pol,'Gd': M_Gd,'Ge':M_Ge,'Si':M_Si}
     O_spec_n,O_spec_g=Layers(O_spec_n,O_spec_g,(mater[typs[i]]),int(thicks[i]))
 
 Details="-->|"
