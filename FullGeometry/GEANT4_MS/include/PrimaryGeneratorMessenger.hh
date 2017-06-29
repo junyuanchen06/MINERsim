845e9e4757ae38f34c01f6fe2652f7509c979381
juntingh_imac <junting.huang@gmail.com>
2017-06-29 15:45:32 -0500

    add primarygeneratormessenger

---
+++ FullGeometry/GEANT4_MS/include/PrimaryGeneratorMessenger.hh
@@ -0,0 +1,34 @@
+#ifndef PrimaryGeneratorMessenger_h
+#define PrimaryGeneratorMessenger_h 1
+
+#include "G4UImessenger.hh"
+#include "globals.hh"
+
+class PrimaryGeneratorAction;
+class G4UIdirectory;
+class G4UIcmdWithAString;
+class G4UIcmdWithoutParameter;
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+class PrimaryGeneratorMessenger: public G4UImessenger
+{
+  public:
+    PrimaryGeneratorMessenger(PrimaryGeneratorAction*);
+   ~PrimaryGeneratorMessenger();
+
+    void SetNewValue(G4UIcommand*, G4String);
+
+  private:
+    PrimaryGeneratorAction*      Action;
+    G4UIdirectory*               CRYDir;
+    G4UIcmdWithAString*          FileCmd;
+    G4UIcmdWithAString*          InputCmd;
+    G4UIcmdWithoutParameter*     UpdateCmd;
+    std::string* MessInput;
+};
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#endif
+
