79aa2135b1701aee0a0a4f4b656653dc55f934a9
juntingh_imac <junting.huang@gmail.com>
2017-07-06 15:49:51 -0500

    set random seed in macro, run gui.mac in vis.mac

--- FullGeometry/GEANT4_MS/MINERsim.cc
+++ FullGeometry/GEANT4_MS/MINERsim.cc
@@ -125,47 +125,36 @@ int main(int argc,char** argv) {
   pgs2.PrepareImportanceSampling(G4IStore::GetInstance(pdet->GetName()),0);
   pgs2.Configure();
 
-
 #ifdef G4VIS_USE
-  // visualization manager
   G4VisManager* visManager = new G4VisExecutive;
   visManager->Initialize();
 #endif
-    
-  // get the pointer to the User Interface manager 
-  G4UImanager* UImanager = G4UImanager::GetUIpointer();  
 
-  if (argc!=1)   // batch mode
-    {
-      G4String command = "/control/execute ";
-      G4String fileName = argv[1];
-      UImanager->ApplyCommand(command+fileName);    
-    }
-  else          // interactive mode : define UI session
-    {
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+  if (argc != 1) {
+    G4String fileName = argv[1];
+    UImanager->ApplyCommand("/control/execute " + fileName);
+  }
+  else {
 #ifdef G4UI_USE
-     G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
 #ifdef G4VIS_USE
     UImanager->ApplyCommand("/control/execute vis.mac");
-    UImanager->ApplyCommand("/control/execute gui.mac");
-#endif             
-     ui->SessionStart();
-     delete ui;
 #endif
-     
+    ui->SessionStart();
+    delete ui;
+#endif
+
 #ifdef G4VIS_USE
-     delete visManager;
-#endif    
-    }
+    delete visManager;
+#endif
+  }
 
-
-  // job termination
-  //
   G4GeometryManager::GetInstance()->OpenGeometry();
   pgs.ClearSampling();
   pgs2.ClearSampling();
   delete runManager;
-  
+
   return 0;
 }
 
